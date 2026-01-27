95803431de8dc558e430a0c97d602e31d5f02ebd
Maxim Isachenko <maksim.isachanka@f-suite.com>
2026-01-27 12:22:58 +0300

    Reconfiguring the database

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,5 +1,4 @@
-// ecosystem.config.cjs
-require('dotenv').config(); // <-- подключаем dotenv
+require('dotenv').config();
 
 module.exports = {
   apps: [
@@ -7,19 +6,11 @@ module.exports = {
       name: 'altasales-backend',
       script: 'dist/main.js',
       cwd: '/var/www/altasales/altasales-api',
-      instances: 1,
-      autorestart: true,
-      watch: false,
-      exec_mode: 'fork',
-      max_memory_restart: '1G',
       env: {
         PORT: process.env.PORT || 3548,
         NODE_ENV: process.env.NODE_ENV || 'production',
-        DB_HOST: process.env.DB_HOST || 'localhost',
-        DB_PORT: process.env.DB_PORT || 5432,
-        DB_USER: process.env.DB_USER || 'altasales_user',
-        DB_PASSWORD: process.env.DB_PASSWORD || 'maimchik002',
-        DB_NAME: process.env.DB_NAME || 'altasales',
+
+        DATABASE_URL: process.env.DATABASE_URL,
       },
     },
   ],
--- src/config/database.module.ts
+++ src/config/database.module.ts
@@ -10,11 +10,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
       inject: [ConfigService],
       useFactory: (config: ConfigService) => ({
         type: 'postgres',
-        host: config.get<string>('DB_HOST') || 'localhost',
-        port: Number(config.get<string>('DB_PORT') || 5432),
-        username: config.get<string>('DB_USER') || 'altasales_user',
-        password: config.get<string>('DB_PASSWORD') || 'maimchik002',
-        database: config.get<string>('DB_NAME') || 'altasales',
+        url: config.get<string>('DATABASE_URL'),
         ssl: false,
         entities: [__dirname + '/../**/*.entity{.ts,.js}'],
         synchronize: !(config.get('NODE_ENV') === 'production'),
