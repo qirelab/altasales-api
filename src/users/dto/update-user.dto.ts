6325a73005759bfbad4e2758c4ed3f6a170964e7
MoskKir <47816850+MoskKir@users.noreply.github.com>
2026-03-06 07:21:39 -0500

    A116 add ability logout from survey page (#17)

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,4 +1,16 @@
 import { PartialType } from '@nestjs/swagger';
+import { ApiPropertyOptional } from '@nestjs/swagger';
+import { IsOptional, IsString } from 'class-validator';
 import { CreateUserDto } from './create-user.dto.js';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) { }
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+  @ApiPropertyOptional({ example: 'Doe', description: 'User last name' })
+  @IsOptional()
+  @IsString()
+  lastName?: string;
+
+  @ApiPropertyOptional({ example: '+71234567890', description: 'User phone number' })
+  @IsOptional()
+  @IsString()
+  phoneNumber?: string;
+}
