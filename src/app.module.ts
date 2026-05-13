a66e6f05b69d7addd69d13567c8a448b5916960e
Maxim Isachenko <maksim.isachanka@f-suite.com>
2026-05-13 13:50:56 +0300

    A211: Add email notifications to admins when user completes questionnaire

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,6 +12,8 @@ import { QuestionnairesModule } from './questionnaires/questionnaires.module';
 import { FilesModule } from './files/files.module';
 import { CartModule } from './cart/cart.module';
 import { RecommendationsModule } from './recommendations/recommendations.module';
+import { MailModule } from './mail/mail.module';
+
 
 @Module({
   imports: [
@@ -28,6 +30,7 @@ import { RecommendationsModule } from './recommendations/recommendations.module'
     FilesModule,
     CartModule,
     RecommendationsModule,
+    MailModule,
   ],
 })
 export class AppModule { }
